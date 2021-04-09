c166a1076da569ec88aaba2d91bb84339957379a
Robert Clark <rbclark@mitre.org>
2021-04-09 16:48:16 -0400

    Fetch inspec inputs from OSCAL component if any appropriate inputs are available

--- lib/inspec-oscal/input.rb
+++ lib/inspec-oscal/input.rb
@@ -1,16 +1,19 @@
 require "json"
+require "jsonpath"
 
 # See https://github.com/inspec/inspec/blob/master/docs/dev/plugins.md#implementing-input-plugins
 
 module InspecPlugins::Oscal
   class Input < Inspec.plugin(2, :input)
 
-    attr_reader :extension_schema_path
+    attr_reader :plugin_conf
     attr_reader :extension_schema
     attr_reader :model_path
     attr_reader :model
     attr_reader :priority
     attr_reader :logger
+    attr_reader :component_props
+    attr_reader :component_set_params
 
     def initialize
       @plugin_conf = Inspec::Config.cached.fetch_plugin_config("inspec-oscal")
@@ -18,44 +21,51 @@ module InspecPlugins::Oscal
       @logger = Inspec::Log
       logger.debug format("inspec-oscal plugin version %s", VERSION)
 
-      @extension_schema_path = fetch_plugin_setting("extension_schema_path", "extension.json")
-      @model_path = fetch_plugin_setting("model_path", "component.json")
+      extension_schema_path = fetch_plugin_setting("extension_schema_path", "extension.json")
+      @extension_schema = JSON.parse(open(extension_schema_path).read)
 
-      fd = open(extension_schema_path)
-      @extension_schema = JSON.parse(fd.read)
+      model_path = fetch_plugin_setting("model_path", "component.json")
+      @model = JSON.parse(open(model_path).read)
 
-      fd = open(model_path)
-      @model_path = JSON.parse(fd.read)
-    end
+      props_json_path = JsonPath.new("$..components..control-implementations..implemented-requirements..props[?(@.ns==#{namespace})][?(@.name==inspec)]")
+      @component_props = props_json_path.on(@model).flatten
 
-    # What priority should an input value recieve from us?
-    # This plgin does not currently allow setting this on a per-input basis,
-    # so they all recieve the same "default" value.
-    # Implements https://github.com/inspec/inspec/blob/master/docs/dev/plugins.md#default_priority
-    def default_priority
-      priority
+      set_parameters_json_path = JsonPath.new('$..components..control-implementations..implemented-requirements..set-parameters')
+      @component_set_params = set_parameters_json_path.on(@model).reduce({}, :merge)
     end
 
     def list_inputs
-        @extension_schema.fetch('extensions', {})
+      @extension_schema.fetch('extensions', {})
     end
 
     def namespace
-        @extension_schema
-            .fetch('extensions', {})
-            .fetch('extension-namespace', {})
-            .fetch('ns', {})
+      @extension_schema.dig('extensions', 'extension-namespace', 'ns') || '*'
     end
 
+    def fetch(profile_name, input_name)
+      results = @component_props.select { |obj| obj['class'] == input_name }
+      if results.length == 1
+        return resolve_references(results.first['value'])
+      end
+    end
+
+    private
+
+    def resolve_references(variable_or_value)
+      if @component_set_params.has_key?(variable_or_value)
+        return @component_set_params[variable_or_value]['value']
+      else # No matching component_set_params found, isn't a variable, just return the value
+        return variable_or_value
+      end
+    end
 
     def fetch_plugin_setting(setting_name, default = nil)
-        env_var_name = "INSPEC_OSCAL_#{setting_name.upcase}"
-        ENV[env_var_name] || plugin_conf[setting_name] || default
-      end
-  
-      def fetch_vault_setting(setting_name)
-        ENV[setting_name.upcase] || plugin_conf[setting_name]
-      end
+      env_var_name = "INSPEC_OSCAL_#{setting_name.upcase}"
+      ENV[env_var_name] || plugin_conf[setting_name] || default
+    end
 
+    def fetch_vault_setting(setting_name)
+      ENV[setting_name.upcase] || plugin_conf[setting_name]
+    end
   end
 end
