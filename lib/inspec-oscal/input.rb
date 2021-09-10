b7bb616ab8831771140dadb53e5d583bf1aa1f87
Robert Clark <rbclark@mitre.org>
2021-09-10 09:50:49 -0400

    Update to the latest OSCAL schema, update structure based on work with atlasity

--- lib/inspec-oscal/input.rb
+++ lib/inspec-oscal/input.rb
@@ -21,31 +21,44 @@ module InspecPlugins::Oscal
       @logger = Inspec::Log
       logger.debug format("inspec-oscal plugin version %s", VERSION)
 
-      extension_schema_path = fetch_plugin_setting("extension_schema_path", "extension.json")
-      @extension_schema = JSON.parse(open(extension_schema_path).read)
+      # The schema is currently disabled for simplicity and due to a lack of use by implementers
+      # extension_schema_path = fetch_plugin_setting("extension_schema_path", "extension.json")
+      # @extension_schema = JSON.parse(open(extension_schema_path).read)
 
       model_path = fetch_plugin_setting("model_path", "component.json")
       @model = JSON.parse(open(model_path).read)
 
-      props_json_path = JsonPath.new("$..components..control-implementations..implemented-requirements..props[?(@.ns==#{namespace})][?(@.name==inspec)]")
+      props_json_path = JsonPath.new("$..components..control-implementations..implemented-requirements..props[?(@.ns==#{namespace})]")
       @component_props = props_json_path.on(@model).flatten
 
       set_parameters_json_path = JsonPath.new('$..components..control-implementations..implemented-requirements..set-parameters')
-      @component_set_params = set_parameters_json_path.on(@model).reduce({}, :merge)
+
+      component_set_params_result = set_parameters_json_path.on(@model)
+      @component_set_params = if component_set_params_result.is_a?(Hash)
+                                component_set_params_result.reduce({}, :merge)
+                              else
+                                component_set_params_result.flatten.map do |params|
+                                  [params['param-id'], (params['values'].length.eql?(1) ? params['values'].first : params['values'])]
+                                end.to_h
+                              end
     end
 
-    def list_inputs
-      @extension_schema.fetch('extensions', {})
+    # Given a profile name, list all input names for which the plugin
+    # would offer a response.
+    def list_inputs(_profile)
+      @component_props.map { |obj| obj['class'] }
     end
 
     def namespace
-      @extension_schema.dig('extensions', 'extension-namespace', 'ns') || '*'
+      'https://mitre.org/ns/inspec'
     end
 
     def fetch(profile_name, input_name)
       results = @component_props.select { |obj| obj['class'] == input_name }
-      if results.length == 1
-        return resolve_references(results.first['value'])
+      if results.length > 0
+        result = results.first
+        resolved_result = resolve_references(results.first['value'])
+        return convert_to_type(resolved_result, result['remarks'])
       end
     end
 
@@ -56,6 +69,19 @@ module InspecPlugins::Oscal
         return @component_set_params[variable_or_value]['value']
       else # No matching component_set_params found, isn't a variable, just return the value
         return variable_or_value
+      end
+    end
+
+    # Converts the resolved input into the type provided in remarks, defaulting to passing the value through
+    # with no modification
+    def convert_to_type(input, type)
+      case type
+      when "integer"
+        return input.to_i
+      when "array"
+        return JSON.load(input)
+      else
+        return input
       end
     end
 
